1de15b542eebfefd1c264deb5265eafc3241a537
hUwUtao <datn4847@gmail.com>
2022-04-07 11:22:17 +0700

    oof

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,26 @@
 import React, { StrictMode as StrictDOM } from 'react'
 import ReactDOM from 'react-dom'
+import env from './env'
 
 import './util/setupTests'
-import './util/analytics'
-import './util/sentry'
 
 import './styles/index.scss'
 
 import App from './App'
+
+import ReactGA from 'react-ga'
+
+import * as Sentry from '@sentry/react'
+import { BrowserTracing } from '@sentry/tracing'
+
+ReactGA.initialize(env.gaTracker)
+ReactGA.pageview(window.location.pathname + window.location.search)
+
+Sentry.init({
+  dsn: env.sentryDSN,
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+})
 
 ReactDOM.render(
   <StrictDOM>
