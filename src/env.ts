54374e0e3a9451caa267174dc1f1d5c14c4c9010
Nguyen Dat <datn4847@gmail.com>
2022-04-07 11:27:00 +0700

    succ

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 const Config: IConfig = {
   sentryDSN:
     'https://1895d60ede6645d19ed402d315a05362@o1192891.ingest.sentry.io/6314654',
-  gaTracker: 'G-Z5FRBVXF84',
+  gaTracker: 'G-R95CS3S3MK',
 }
 interface IConfig {
   sentryDSN: string
