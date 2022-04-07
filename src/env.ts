e1e4b78a47d2a13273161ec7ab39226badb78712
hUwUtao <datn4847@gmail.com>
2022-04-07 11:09:12 +0700

    sussy

---
+++ src/env.ts
@@ -0,0 +1,10 @@
+const Config: IConfig = {
+  sentryDSN:
+    'https://1895d60ede6645d19ed402d315a05362@o1192891.ingest.sentry.io/6314654',
+  gaTracker: 'G-Z5FRBVXF84',
+}
+interface IConfig {
+  sentryDSN: string
+  gaTracker: string
+}
+export default Config
